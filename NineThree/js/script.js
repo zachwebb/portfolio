a13f287b84f6c7390dd7b03a20a687bb85beefd0
Zach Webb <zachary.t.webb@gmail.com>
2016-02-02 12:02:25 -0600

    Updates to CSS, HTML, JS, added JSON for future use.

--- NineThree/js/script.js
+++ NineThree/js/script.js
@@ -1,42 +1,40 @@
 $(function(){
 
-$(document).ready(function(){
+	$(document).ready(function(){
 
-	$("#gallery").unitegallery({
-		lightbox_arrows_position: "outside",
-		lightbox_overlay_opacity:.8,
-		lightbox_textpanel_enable_description: true,
-		lightbox_type: "compact",
-		theme_autoplay: true,
-		tile_enable_icons: false,
-		tile_overlay_color: "#ffffff",
-		tiles_justified_row_height: 200,
-		tiles_type:"justified",
+		$("#gallery").unitegallery({
+			lightbox_arrows_position: "outside",
+			lightbox_overlay_opacity:.8,
+			lightbox_textpanel_enable_description: true,
+			lightbox_type: "compact",
+			theme_autoplay: true,
+			tile_enable_icons: false,
+			tile_overlay_color: "#ffffff",
+			tiles_justified_row_height: 200,
+			tiles_type:"justified",
+			
+			// gallery_width:"100%",
+			// grid_num_rows:9999,
+			// grid_padding:0,
+			// grid_space_between_cols:0,
+			// grid_space_between_rows:0,
+			// lightbox_arrows_position: "outside",
+			// lightbox_overlay_opacity:.8,
+			// lightbox_type: "compact",
+			// lightbox_textpanel_enable_description: true,
+			// tile_overlay_color: "#ffffff",
+			// tile_enable_border:false,
+			// tile_enable_icons: false,
+			// tile_enable_shadow:false,
+			// tile_height:250,
+			// tile_width:250,
+		});
 		
-		// gallery_width:"100%",
-		// grid_num_rows:9999,
-		// grid_padding:0,
-		// grid_space_between_cols:0,
-		// grid_space_between_rows:0,
-		// lightbox_arrows_position: "outside",
-		// lightbox_overlay_opacity:.8,
-		// lightbox_type: "compact",
-		// lightbox_textpanel_enable_description: true,
-		// tile_overlay_color: "#ffffff",
-		// tile_enable_border:false,
-		// tile_enable_icons: false,
-		// tile_enable_shadow:false,
-		// tile_height:250,
-		// tile_width:250,
 	});
-	
-});
 
+	$('.navlink').on('click', function(event) {
 
-
-$('.navlink').on('click', function(event) {
-
-	    var target = $( $(this).attr('href') );
+		var target = $( $(this).attr('href') );
 
 	    if( target.length ) {
 	        event.preventDefault();
