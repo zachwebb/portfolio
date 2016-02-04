f3adaf20ee1f6629eca9671e319db33316bf4312
Zach Webb <zachary.t.webb@gmail.com>
2016-02-03 21:16:41 -0600

    Lots of changes.

--- NineThree/js/script.js
+++ NineThree/js/script.js
@@ -1,5 +1,36 @@
 $(function(){
 
+	var STUDENT_METHOD ={
+
+		handlerData:function(resJSON){
+
+		var templateSource = $("#portfolio-content").html(),
+
+		template = Handlebars.compile(templateSource),
+
+		studentHTML = template(resJSON);
+
+		$('#main-content').html(studentHTML);
+
+		},
+		loadPortfolio : function(){
+
+		$.ajax({
+		url:"http://localhost:63342/HandleBarDemo/data/studentData.json",
+		method:'get',
+		success:this.handlerData
+
+		})
+		}
+		};
+
+		$(document).ready(function(){
+
+		STUDENT_METHOD.loadStudentData();
+
+		});
+
+
 	$('a.gallery').colorbox({rel:'a.gallery', transition:"none", maxWidth:'95%', maxHeight:'95%'});
 	$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390, maxWidth:'95%', maxHeight:'95%'});
 	$('.navlink').on('click', function(event) {
